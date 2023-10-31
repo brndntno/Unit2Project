9d47ad9269ce3b2514749ebf8dcb9c51d7126f77
brndntno <145706773+brndntno@users.noreply.github.com>
2023-10-31 13:54:51 -0400

    Update LinearEquation.java

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -8,17 +8,29 @@ public class LinearEquation {
     private int y1;
     private int x2;
     private int x1;
-    private double slope;
-    private double yIntercept;
-    private double distance;
-    private String equation;
-    public double xValue;
+    //private double slope;
+    //private double yIntercept;
+    //private double distance;
+    //private String equation;
     private double coordinateForX;
 
     // constructor
     public LinearEquation(String coordinate1, String coordinate2) {
         this.coordinate1 = coordinate1;
         this.coordinate2 = coordinate2;
+        y2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf(",") + 2, coordinate2.indexOf(")")));
+        y1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf(",") + 2, coordinate1.indexOf(")")));
+        x2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf("(") + 1, coordinate2.indexOf(",")));
+        x1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf("(") + 1, coordinate1.indexOf(",")));
+    }
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        coordinate1 = "(" + x1 + ", " + y1 + ")";
+        coordinate2 = "(" + x2 + ", " + y2 + ")";
     }
 
 
@@ -33,43 +45,37 @@ public class LinearEquation {
 
 
     public double slope() {
-        y2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf(",") + 2, coordinate2.indexOf(")")));
-        y1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf(",") + 2, coordinate1.indexOf(")")));
-        x2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf("(") + 1, coordinate2.indexOf(",")));
-        x1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf("(") + 1, coordinate1.indexOf(",")));
-        slope = (double) (y2 - y1) / (double) (x2 - x1);
+        double slope = (double) (y2 - y1) / (double) (x2 - x1);
         return slope;
     }
 
 
     public double yIntercept() {
-        yIntercept = y1 - (slope * x1);
+        double yIntercept = y1 - (slope() * x1);
         return yIntercept;
     }
 
 
-    public double getDistance() {
-        distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    public double distance() {
+        double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         return distance;
     }
 
-    public String getEquation() {
-        equation = "y = " + (y2 - y1) + "/" + (x2 - x1) + "x + " + yIntercept;
+    public String equation() {
+        String equation = "y = " + (y2 - y1) + "/" + (x2 - x1) + "x + " + yIntercept();
         return equation;
     }
 
-    public void lineInfo() {
-        System.out.println("The two points are: " + coordinate1 + " and " + coordinate2 + "\n" +
-        "The equation of the line between these points is: " + equation + "\n" +
-        "The slope of this line is: " + slope + "\n" +
-        "The y-intercept of the line is: " + yIntercept + "\n" +
-        "The distance between the points is: " + distance);
-        System.out.println();
+    public String lineInfo() {
+        return "The two points are: " + coordinate1 + " and " + coordinate2 + "\n" +
+                "The equation of the line between these points is: " + equation() + "\n" +
+                "The slope of this line is: " + slope() + "\n" +
+                "The y-intercept of the line is: " + yIntercept() + "\n" +
+                "The distance between the points is: " + distance();
     }
 
-    public double getCoordinateForX() {
-        coordinateForX = slope * xValue + yIntercept;
+    public double coordinateForX(double xValue) {
+        coordinateForX = slope() * xValue + yIntercept();
         return coordinateForX;
     }
 }
-
