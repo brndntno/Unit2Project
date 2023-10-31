e02ab06c2c75aca368398469a1ada430b45e0d00
brndntno <145706773+brndntno@users.noreply.github.com>
2023-10-31 02:25:43 -0400

    Update LinearEquationLogic.java

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -17,6 +17,7 @@ public class LinearEquationLogic {
         String coordinate1 = myScanner.nextLine();
         System.out.print("Enter coordinate 2: ");
         String coordinate2 = myScanner.nextLine();
+        System.out.println();
         linearEquation = new LinearEquation(coordinate1, coordinate2);
         linearEquation.getCoordinate1();
         linearEquation.getCoordinate2();
@@ -24,17 +25,28 @@ public class LinearEquationLogic {
         linearEquation.yIntercept();
         linearEquation.getDistance();
         linearEquation.getEquation();
-        linearEquation.lineInfo();
-        System.out.print("Enter a value for x: ");
-        double xValue = Integer.parseInt(myScanner.nextLine());
-        System.out.println("The point on the line is (" + xValue + ", " + linearEquation.getCoordinateForX() + ")");
+        if (linearEquation.getCoordinate1().substring(1, linearEquation.getCoordinate1().indexOf(",")).equals(linearEquation.getCoordinate2().substring(1, linearEquation.getCoordinate2().indexOf(",")))) {
+            System.out.println("These points are on a vertical line: x = " + linearEquation.getCoordinate1().substring(1, linearEquation.getCoordinate1().indexOf(",")));
+            System.out.println();
+        } else if (linearEquation.getCoordinate1().substring(linearEquation.getCoordinate1().indexOf(",") + 2, linearEquation.getCoordinate1().indexOf(")")).equals(linearEquation.getCoordinate1().substring(linearEquation.getCoordinate2().indexOf(",") + 2, linearEquation.getCoordinate2().indexOf(")")))) {
+            System.out.println("These points are on a horizontal line: y = " + linearEquation.yIntercept());
+        } else {
+            linearEquation.lineInfo();
+            System.out.print("Enter a value for x: ");
+            linearEquation.xValue = Integer.parseInt(myScanner.nextLine());
+            System.out.println();
+            System.out.println("The point on the line is (" + linearEquation.xValue + ", " + linearEquation.getCoordinateForX() + ")");
+            System.out.println();
+        }
     }
 
     private void getData() {
         System.out.print("Would you like to enter another pair of coordinates? y/n: ");
         String yOrN = myScanner.nextLine();
-        while (yOrN.equals("y")) {
-            getLineInfo();
+        if (yOrN.equals("y")) {
+            start();
+        } else {
+            System.out.println("Thank you for using the slope calculator, goodbye!");
         }
     }
 }
